dd9d93f8bb2fa4d2942050deea4c7af17819c38c
jiashuaizhang@126.com <jiashuaizhang@126.com>
2021-12-13 21:30:00 +0800

    db文件首次运行时自动解压

--- yyets-history/src/main/java/com/zhangjiashuai/yyetshistory/config/AppConfig.java
+++ yyets-history/src/main/java/com/zhangjiashuai/yyetshistory/config/AppConfig.java
@@ -1,13 +1,26 @@
 package com.zhangjiashuai.yyetshistory.config;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.IORuntimeException;
+import cn.hutool.core.net.URLDecoder;
+import cn.hutool.extra.compress.extractor.SevenZExtractor;
 import com.zhangjiashuai.yyetshistory.repository.ResourceRepository;
 import com.zhangjiashuai.yyetshistory.repository.impl.ResourceRepositoryJdbcImpl;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.StandardCopyOption;
+
+@Slf4j
 @Configuration
 public class AppConfig {
 
@@ -21,5 +34,47 @@ public class AppConfig {
     @ConfigurationProperties(prefix = "yyets-history")
     public YyetsHistoryProperties yyetsHistoryConfig() {
         return new YyetsHistoryProperties();
+    }
+
+    @Bean
+    public DataSource dataSource(DataSourceProperties properties) {
+        extractDb7zFile();
+        return DataSourceBuilder.create().driverClassName(properties.getDriverClassName())
+                .url(properties.getUrl()).build();
+    }
+
+    private void extractDb7zFile() {
+        final String moduleName = "/yyets-history";
+        String projectPath = System.getProperty("user.dir");
+        projectPath = projectPath.replaceAll("\\\\", "/");
+        if(!projectPath.endsWith(moduleName)) {
+            projectPath = projectPath + moduleName;
+        }
+        String staticResourcePath = projectPath + "/src/main/resources/static/";
+        String dbFilePath = staticResourcePath + "yyets.db";
+        File dbFile = new File(dbFilePath);
+        if(!dbFile.exists()) {
+            String db7zFilePath = staticResourcePath + "yyets.7z";
+            File db7zFile = new File(db7zFilePath);
+            if(!db7zFile.exists()) {
+                throw new IORuntimeException("数据源文件缺失并且无法找到压缩包: " + db7zFilePath);
+            }
+            log.info("开始解压数据源文件: {}", db7zFilePath);
+            try (SevenZExtractor sevenZExtractor = new SevenZExtractor(db7zFile)) {
+                sevenZExtractor.extract(new File(staticResourcePath));
+                if(dbFile.exists()) {
+                    log.info("数据源文件自动解压完成: {}", dbFilePath);
+                    String classPath = getClass().getResource("/static").getPath();
+                    classPath = URLDecoder.decode(classPath, StandardCharsets.UTF_8);
+                    FileUtil.copyFile(dbFile, new File(classPath), StandardCopyOption.REPLACE_EXISTING);
+                    log.info("复制数据源文件至classpath: {}", classPath);
+                } else {
+                    throw new IORuntimeException("未知的解压异常: " + db7zFilePath);
+                }
+            } catch (Exception e) {
+                String msg = "数据源文件自动解压失败，请手动解压文件: resources/static/yyets.7z";
+                throw new IORuntimeException(msg, e);
+            }
+        }
     }
 }
--- yyets-history/src/test/java/com/zhangjiashuai/yyetshistory/SimpleTest.java
+++ yyets-history/src/test/java/com/zhangjiashuai/yyetshistory/SimpleTest.java
@@ -1,7 +1,12 @@
 package com.zhangjiashuai.yyetshistory;
 
+import cn.hutool.core.lang.Assert;
+import cn.hutool.core.net.URLDecoder;
+import cn.hutool.extra.compress.extractor.SevenZExtractor;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.util.LinkedHashSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -29,5 +34,31 @@ public class SimpleTest {
             });
         }
         executorService.shutdown();
+    }
+
+    @Test
+    public void testExtract7z() {
+        String projectPath = System.getProperty("user.dir");
+        String staticResourcePath = projectPath + "/src/main/resources/static/";
+        String db7zFilename = "yyets.7z";
+        File db7zFile = new File(staticResourcePath + db7zFilename);
+        System.out.println(db7zFile.exists());
+        try (SevenZExtractor sevenZExtractor = new SevenZExtractor(db7zFile)) {
+            sevenZExtractor.extract(new File(staticResourcePath));
+        }
+        File dbFile = new File(staticResourcePath + "yyets.db");
+        Assert.isTrue(dbFile.exists());
+    }
+
+    @Test
+    public void testUriDecode() {
+        String path = "/E:/%e4%bb%a3%e7%a0%81/%e9%9b%b6%e7%a2%8e/yyets-snapshot/yyets-history/target/classes/static";
+        String decode = URLDecoder.decode(path, StandardCharsets.UTF_8);
+        System.out.println(decode);
+        String decodeAgain = URLDecoder.decode(decode, StandardCharsets.UTF_8);
+        Assert.isTrue(decode.equals(decodeAgain));
+        File file = new File(decode);
+        System.out.println(file.isDirectory());
+        System.out.println(file.getAbsolutePath());
     }
 }
